a56300c3f6c57afb61d7af9782a517d958563d86
mja1110 <truongle11102000@gmail.com>
2024-04-13 19:13:30 +0200

    update

--- graph.py
+++ graph.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+
 # Load the Excel file
 file_path = 'failure time distribution.xlsx'  # Replace 'path_to_your_file.xlsx' with the actual path to your file
 df = pd.read_excel(file_path)
@@ -20,9 +22,9 @@ rows_POSTE_DE_CONTROLE, columns_POSTE_DE_CONTROLE = result_POSTE_DE_CONTROLE
 # print(columns)		# return the index of columns of POSTE DE CONTRÔLE (numpy.ndarray)
 
 data_failure_time_distribution_POSTE_DE_CONTROLE = df.loc[rows_POSTE_DE_CONTROLE, "failure time distribution"] # pandas.core.series.Series
-# print(failure_time_distribution)
-# print(type(failure_time_distribution))
-# print(failure_time_distribution.shape)
+print(failure_time_distribution)
+print(type(failure_time_distribution))
+print(failure_time_distribution.shape)
 
 
 # --------------------------------- CONNECTEURS ---------------------------------
@@ -41,7 +43,7 @@ data_failure_time_distribution_POSTE_09 = df.loc[rows_POSTE_09, "failure time di
 result_POSTE_04 = np.where(df == "POSTE 04  : EMMANCHEMENTS ROULEMENTS (PRESSE)")
 rows_POSTE_04, columns_POSTE_04 = result_POSTE_04
 data_failure_time_distribution_POSTE_04 = df.loc[rows_POSTE_04, "failure time distribution"]
-print(len(data_failure_time_distribution_POSTE_04))
+
 
 # --------------------------------- CONVOYEURS ---------------------------------
 result_CONVOYEURS = np.where(df == "CONVOYEURS")
@@ -53,32 +55,23 @@ data_failure_time_distribution_CONVOYEURS = df.loc[rows_CONVOYEURS, "failure tim
 result_LIGNE_DE_MONTAGE_MOTG02 = np.where(df == "LIGNE DE MONTAGE MOTG02")
 rows_LIGNE_DE_MONTAGE_MOTG02, columns_LIGNE_DE_MONTAGE_MOTG02 = result_LIGNE_DE_MONTAGE_MOTG02
 data_failure_time_distribution_LIGNE_DE_MONTAGE_MOTG02 = df.loc[rows_LIGNE_DE_MONTAGE_MOTG02, "failure time distribution"]
+# print(len(data_failure_time_distribution_LIGNE_DE_MONTAGE_MOTG02))
 
 
 # Creating the one-dimensional strip plot
+x_label = [data_failure_time_distribution_POSTE_DE_CONTROLE, data_failure_time_distribution_CONNECTEURS, data_failure_time_distribution_POSTE_09, data_failure_time_distribution_POSTE_04, data_failure_time_distribution_CONVOYEURS, data_failure_time_distribution_LIGNE_DE_MONTAGE_MOTG02]
+y_label = ['POSTE DE CONTRÔLE', 'CONNECTEURS', 'POSTE 09 : MONTAGE CÔTÉ A (RETOURNEMENTS)', 'POSTE 04  : EMMANCHEMENTS ROULEMENTS (PRESSE)', 'CONVOYEURS', 'LIGNE DE MONTAGE MOTG02']
+length_y = [len(data_failure_time_distribution_POSTE_DE_CONTROLE), len(data_failure_time_distribution_CONNECTEURS), len(data_failure_time_distribution_POSTE_09), len(data_failure_time_distribution_POSTE_04), len(data_failure_time_distribution_CONVOYEURS), len(data_failure_time_distribution_LIGNE_DE_MONTAGE_MOTG02)]
+data = {
+	'x_label': np.concatenate(x_label),
+	'y_label': np.repeat(y_label, length_y)
+}
 
+plt.figure(figsize=(20, 6))
+sns.stripplot(x='x_label', y='y_label', data=data, jitter=False, color='red', size=4)
+plt.title('Distribution of Failure on Each Component')
+plt.xlabel('Time')
+plt.ylabel('Component')
+plt.xlim(-10, 6000)
 
-
-
-
-
-
-
-
-
-
-# Creating the one-dimensional scatter plot
-plt.figure(figsize=(10, 1))  # Setting the figure size to emphasize the one-dimensionality
-plt.scatter(data_failure_time_distribution_POSTE_09, np.zeros_like(data_failure_time_distribution_POSTE_09), color='blue', marker='x')  # Scatter plot along one axis
-plt.yticks([])  # Hide y-axis ticks
-plt.gca().axes.get_yaxis().set_visible(False)  # Hide the y-axis
-plt.title('Failure Time Distribution of POSTE DE CONTRÔLE')
-plt.xlabel('Data Points')
 plt.show()
-
-
-
-    
-
-
-
