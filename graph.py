f6ca49eaecbfebe2321df2e8f79fab1864115ade
Mja1110 <truongle11102000@gmail.com>
2024-04-15 14:24:49 +0200

    add timeline graph

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import plotly.express as px
 
 # Load the Excel file
 file_path = 'failure time distribution.xlsx'  # Replace 'path_to_your_file.xlsx' with the actual path to your file
@@ -9,8 +10,10 @@ df = pd.read_excel(file_path)
 
 component = df['component']
 failure_time_distribution = df['failure time distribution']
+downtime = df['downtime']
 # print(component)
 # print(failure_time_distribution)
+# print(downtime)
 
 
 # --------------------------------- POSTE DE CONTRÔLE ---------------------------------
@@ -22,9 +25,10 @@ rows_POSTE_DE_CONTROLE, columns_POSTE_DE_CONTROLE = result_POSTE_DE_CONTROLE
 # print(columns)		# return the index of columns of POSTE DE CONTRÔLE (numpy.ndarray)
 
 data_failure_time_distribution_POSTE_DE_CONTROLE = df.loc[rows_POSTE_DE_CONTROLE, "failure time distribution"] # pandas.core.series.Series
-print(failure_time_distribution)
-print(type(failure_time_distribution))
-print(failure_time_distribution.shape)
+# print(data_failure_time_distribution_POSTE_DE_CONTROLE)
+# print(type(data_failure_time_distribution_POSTE_DE_CONTROLE))
+# print(data_failure_time_distribution_POSTE_DE_CONTROLE.shape)
+
 
 
 # --------------------------------- CONNECTEURS ---------------------------------
@@ -33,10 +37,12 @@ rows_CONNECTEURS, columns_CONNECTEURS = result_CONNECTEURS
 data_failure_time_distribution_CONNECTEURS = df.loc[rows_CONNECTEURS, "failure time distribution"] 
 
 
+
 # --------------------------------- POSTE 09 : MONTAGE CÔTÉ A (RETOURNEMENTS) ---------------------------------
 result_POSTE_09 = np.where(df == "POSTE 09 : MONTAGE CÔTÉ A (RETOURNEMENTS)")
 rows_POSTE_09, columns_POSTE_09 = result_POSTE_09
 data_failure_time_distribution_POSTE_09 = df.loc[rows_POSTE_09, "failure time distribution"]
+
 
 
 # --------------------------------- POSTE 04  : EMMANCHEMENTS ROULEMENTS (PRESSE) ---------------------------------
@@ -45,10 +51,12 @@ rows_POSTE_04, columns_POSTE_04 = result_POSTE_04
 data_failure_time_distribution_POSTE_04 = df.loc[rows_POSTE_04, "failure time distribution"]
 
 
+
 # --------------------------------- CONVOYEURS ---------------------------------
 result_CONVOYEURS = np.where(df == "CONVOYEURS")
 rows_CONVOYEURS, columns_CONVOYEURS = result_CONVOYEURS
 data_failure_time_distribution_CONVOYEURS = df.loc[rows_CONVOYEURS, "failure time distribution"] 
+
 
 
 # --------------------------------- LIGNE DE MONTAGE MOTG02 ---------------------------------
@@ -56,6 +64,7 @@ result_LIGNE_DE_MONTAGE_MOTG02 = np.where(df == "LIGNE DE MONTAGE MOTG02")
 rows_LIGNE_DE_MONTAGE_MOTG02, columns_LIGNE_DE_MONTAGE_MOTG02 = result_LIGNE_DE_MONTAGE_MOTG02
 data_failure_time_distribution_LIGNE_DE_MONTAGE_MOTG02 = df.loc[rows_LIGNE_DE_MONTAGE_MOTG02, "failure time distribution"]
 # print(len(data_failure_time_distribution_LIGNE_DE_MONTAGE_MOTG02))
+
 
 
 # Creating the one-dimensional strip plot
@@ -66,12 +75,47 @@ data = {
 	'x_label': np.concatenate(x_label),
 	'y_label': np.repeat(y_label, length_y)
 }
-
 plt.figure(figsize=(20, 6))
 sns.stripplot(x='x_label', y='y_label', data=data, jitter=False, color='red', size=4)
 plt.title('Distribution of Failure on Each Component')
 plt.xlabel('Time')
 plt.ylabel('Component')
 plt.xlim(-10, 6000)
+# plt.show()
 
+
+
+
+data_downtime_POSTE_DE_CONTROLE = df.loc[rows_POSTE_DE_CONTROLE, "downtime"]
+data_downtime_CONNECTEURS = df.loc[rows_CONNECTEURS, "downtime"]  
+data_downtime_POSTE_09 = df.loc[rows_POSTE_09, "downtime"]
+data_downtime_POSTE_04 = df.loc[rows_POSTE_04, "downtime"]
+data_downtime_CONVOYEURS = df.loc[rows_CONVOYEURS, "downtime"] 
+data_downtime_LIGNE_DE_MONTAGE_MOTG02 = df.loc[rows_LIGNE_DE_MONTAGE_MOTG02, "downtime"]
+
+
+data_POSTE_DE_CONTROLE = [(x, y) for x, y in zip(data_failure_time_distribution_POSTE_DE_CONTROLE, data_downtime_POSTE_DE_CONTROLE)]
+data_CONNECTEURS = [(x, y) for x, y in zip(data_failure_time_distribution_CONNECTEURS, data_downtime_CONNECTEURS)]
+data_POSTE_09 = [(x, y) for x, y in zip(data_failure_time_distribution_POSTE_09, data_downtime_POSTE_09)]
+data_POSTE_04 = [(x, y) for x, y in zip(data_failure_time_distribution_POSTE_04, data_downtime_POSTE_04)]
+data_CONVOYEURS = [(x, y) for x, y in zip(data_failure_time_distribution_CONVOYEURS, data_downtime_CONVOYEURS)]
+data_LIGNE_DE_MONTAGE_MOTG02 = [(x, y) for x, y in zip(data_failure_time_distribution_LIGNE_DE_MONTAGE_MOTG02, data_downtime_LIGNE_DE_MONTAGE_MOTG02)]
+# print(data_POSTE_DE_CONTROLE)
+
+
+
+# Horizontal bar plot with gaps
+fig, ax = plt.subplots()
+ax.broken_barh(data_POSTE_DE_CONTROLE, (63, 4), facecolors='tab:blue')
+ax.broken_barh(data_CONNECTEURS, (53, 4), facecolors='tab:green')
+ax.broken_barh(data_POSTE_09, (43, 4), facecolors='tab:orange')
+ax.broken_barh(data_POSTE_04, (33, 4), facecolors='tab:red')
+ax.broken_barh(data_CONVOYEURS, (23, 4), facecolors='black')
+ax.broken_barh(data_LIGNE_DE_MONTAGE_MOTG02, (13, 4), facecolors='tab:pink')
+ax.set_xlim(-100, 5500)
+ax.set_ylim(0, 70)
+ax.set_xlabel('Time')
+ax.set_ylabel('Component')
+ax.set_yticks([65, 55, 45, 35, 25, 15], labels=['POSTE DE CONTRÔLE', 'CONNECTEURS', 'POSTE 09 : MONTAGE CÔTÉ A (RETOURNEMENTS)', 'POSTE 04  : EMMANCHEMENTS ROULEMENTS (PRESSE)', 'CONVOYEURS', 'LIGNE DE MONTAGE MOTG02'])     # Modify y-axis tick labels
+ax.grid(True)                                       # Make grid lines visible
 plt.show()
